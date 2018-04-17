abaeb193c931e81b8de9e8a5192dcd2dfc94f87e
Wei Chen <33862008+henrychen222@users.noreply.github.com>
2018-04-17 01:11:45 -0400

    Add files via upload

---
+++ Lab7/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const configRoutes = require('./routes');
+
+
+// body-parser middleware
+
+app.use(bodyParser.json());
+configRoutes(app);
+
+
+app.listen(3000, () => {
+    console.log("Server has started");
+    console.log("Listening on port 3000...");
+});
